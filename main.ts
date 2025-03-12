ec1a4e68ac0bed25378194ae69b1f593578a48ed
Abhiraam Bijumon <abhiraambijumon@gmail.com>
2025-03-11 21:19:34 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     mySprite.setVelocity(0, -200)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    game.gameOver(false)
+})
+let projectile: Sprite = null
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
@@ -22,3 +26,25 @@ mySprite = sprites.create(img`
     `, SpriteKind.Player)
 mySprite.setStayInScreen(true)
 mySprite.ay = 500
+forever(function () {
+    pause(2000)
+    projectile = sprites.createProjectileFromSide(img`
+        . 1 1 1 1 1 1 1 1 1 1 1 . . . . 
+        . 1 f f f f f f f f f 1 1 1 . . 
+        1 1 f f f f f f f f f f f 1 1 . 
+        1 f f f f f f f f f 1 f f f 1 1 
+        1 f f f f f f f f f f f f f f 1 
+        f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f 
+        f f f f f f f f f f f f f f f f 
+        1 f f f f f f f f f f f f f f f 
+        1 f f f f f 1 f f f f f f f f f 
+        1 f f f f f f f f f f f f f f 1 
+        1 1 1 f f f f f f f f f f f 1 1 
+        . . 1 1 f f f f f f f f f 1 1 . 
+        . . . 1 1 1 f f f f f f f 1 . . 
+        . . . . . 1 1 1 f f f 1 1 1 . . 
+        . . . . . . . 1 1 1 1 1 . . . . 
+        `, -50, 0)
+    projectile.bottom = randint(-120, 100)
+})
